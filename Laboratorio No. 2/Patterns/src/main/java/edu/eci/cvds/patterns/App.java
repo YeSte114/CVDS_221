1d7d488950e95ea24c5966e4aeb81278dd04b620
YeSte114 <46783436+YeSte114@users.noreply.github.com>
2022-01-31 17:33:10 -0500

    Final Laboratorio 2

--- Laboratorio No. 2/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Laboratorio No. 2/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -4,10 +4,13 @@ package edu.eci.cvds.patterns;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public static void main(String[] args){
+        if(String.join("",args).equals("")){
+            System.out.println("Hello World!");
+        }
+        else{
+            System.out.println("Hello "+String.join(" ",args)+"!");
+        }
     }
 }
