1ad96d4fdf0be807a19e1c389a41fd93a56f5d81
lrn000 <lngarcia878@gmail.com>
2022-02-16 08:24:08 -0500

    implementación de pruebas

--- Laboratorio No. 4/src/main/java/hangman/test/GameScoreTest.java
+++ Laboratorio No. 4/src/main/java/hangman/test/GameScoreTest.java
@@ -1,10 +1,86 @@
 package hangman.test;
 
-public class GameScoreTest {
+import  org.junit.Assert;
+import hangman.model.*;
 
-    /**
-     *
-     * Original Score
-     * 
-     */
+public class GameScoreTest {
+    private GameScore original = new OriginalScore();
+
+    @Test
+    public void validarCorrectoOrigenScore() {
+        int score = original.calculateScore(1, 1);
+        Assert.assertEquals(score, 90);
+    }
+
+    @Test
+    public void validarIncorrectOrigenScore() {
+        int score = original.calculateScore(1, 11);
+        Assert.assertEquals(score, -10);
+    }
+
+    @Test
+    public void validarIgual100OiginalScore() {
+        int score = original.calculateScore(1, 0);
+        Assert.assertEquals(score, 100);
+    }
+
+    @Test
+    public void validarIgualCeroOriginalScore() {
+        int score = original.calculateScore(1, 10);
+        Assert.assertEquals(score, 0);
+    }
+
+    @Test
+    public void validarIncorrectOrigenScoreNeg() {
+        int score = original.calculateScore(-1, -1);
+        Assert.assertEquals(score, -10);
+    }
+
+
+    //BonusScore
+
+    @Test
+    public void validarIgualCeroBonusScore() {
+        int score = original.calculateScore(0, 0);
+        Assert.assertEquals(score, 0);
+    }
+
+    @Test
+    public void validarIgualDiezBonusScore() {
+        int score = original.calculateScore(1, 0);
+        Assert.assertEquals(score, 10);
+    }
+
+    @Test
+    public void validarIgualCincoBonusScore() {
+        int score = original.calculateScore(1, 0);
+        Assert.assertEquals(score, 5);
+    }
+
+    @Test
+    public void validarIgualInvalidBonusScore() {
+        int score = original.calculateScore(1, 3);
+        Assert.assertEquals(score, -5);
+    }
+
+    //PowerScore
+
+    @Test
+    public void validarIgualCeroPowerScore() {
+        int score = original.calculateScore(0, 0);
+        Assert.assertEquals(score, 0);
+    }
+
+    @Test
+    public void validarIgualCeroPowerScore() {
+        int score = original.calculateScore(0, 0);
+        Assert.assertEquals(score, 0);
+    }
+
+    @Test
+    public void validarIgual500PowerScore() {
+        int score = original.calculateScore(14, 0);
+        Assert.assertEquals(score, 500);
+    }
+
 }
